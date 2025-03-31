e4470d2d18a2d43f294e881b8227c864fc44ffac
labubunews <184888992+labubunews@users.noreply.github.com>
2025-03-31 09:01:21 +0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Fullscreen YouTube Video</title>
+    <style>
+        * {
+            margin: 0;
+            padding: 0;
+            box-sizing: border-box;
+        }
+        body {
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            height: 100vh;
+            background-color: black;
+        }
+        .video-container {
+            width: 100vw;
+            height: 100vh;
+        }
+        iframe {
+            width: 100%;
+            height: 100%;
+            border: none;
+        }
+    </style>
+</head>
+<body>
+    <div class="video-container">
+        <iframe src="https://youtu.be/au2n7VVGv_c?list=RDba7mB8oueCY" 
+                allowfullscreen>
+        </iframe>
+    </div>
+</body>
+</html>
